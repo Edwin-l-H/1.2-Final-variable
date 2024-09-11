4a1ada805ef8aa1f367d816eddbad4309f3549af
Edwin-l-H <edwinlh2789@nycstudents.net>
2024-09-11 13:34:23 +0000

    Pending changes exported from your codespace

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        final int a = 2;
+        double b = 4.1;
+        boolean x = true;
+        String s = " Output = ";
+
+        b = b - a;
+        System.out.println(x + s + b);
+
+    }
+}
